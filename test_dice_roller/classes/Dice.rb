9b12a79bb57cc71655986616a33f05e075374460
David Turgeon <david.turgeon@gmail.com>
2025-07-18 07:13:45 -0600

    Add dice roller

---
+++ test_dice_roller/classes/Dice.rb
@@ -0,0 +1,22 @@
+class Dice
+
+  attr_reader :value
+
+  def initialize
+    roll
+  end
+
+  def roll
+    @value = get_a_random_number
+  end
+
+  def display
+     "[ #{value} ]"
+  end
+
+private
+
+  def get_a_random_number
+    1 + rand(6)
+  end
+end
---
+++ test_dice_roller/init.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+
+require_relative 'classes/Dice'
+require_relative 'classes/DiceSet'
+
+dice_set = DiceSet.new
+
+puts "Welcome to the Dice Roller"
+puts "-" * 26
+puts
+
+puts dice_set.display
+
+loop do 
+  puts
+  puts "Type 'r' for roll or 'q' for quit"
+  roll=gets.chomp
+  if roll == 'q'
+     break
+  end
+  if roll == 'r'
+     dice_set.roll
+     puts dice_set.display
+   end
+end
