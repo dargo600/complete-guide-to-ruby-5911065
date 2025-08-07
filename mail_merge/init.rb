28c9daf0735f0aee1d35afd01f1540eda01ac9bc
Dave Turgeon <david.turgeon@gmail.com>
2025-08-06 20:57:41 -0600

    add mail_merge

--- mail_merge/init.rb
+++ mail_merge/init.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
 
+require 'erb'
+require 'csv'
+
 APP_ROOT = File.expand_path(File.dirname(__FILE__))
 
 def number_to_currency(value)
@@ -7,13 +10,39 @@ def number_to_currency(value)
 end
 
 # Read template file containing the overdue notice
+template_path = File.join(APP_ROOT, 'overdue_notice.txt.erb')
 
 # Read CSV file to get list of overdue notices
+csv_file = File.join(APP_ROOT, 'overdue_list.csv')
 
+# Read template file containing the overdue notice
+template = File.read(template_path)
+
+output_dir = File.join(APP_ROOT, "letters")
+i = 1
 # For each notice:
-
 # Set instance variables as needed
-
 # Use ERB to bind instance variables to the letter template
-
 # Save results as separate files (e.g., "letters/overdue_01.txt")
+CSV.foreach(csv_file, headers: true) do |row|
+  # puts row["Last Name"]
+  @borrowed_by = row["First Name"] + " " + row["Last Name"]
+  @overdue_days = row["Days"].to_i
+  @fee = number_to_currency(@overdue_days * 0.25)
+  @book_title = row["Title"]
+
+  # Use ERB to bind instance variables to the letter template
+  output = ERB.new(template).result(binding)
+
+  num = i < 10 ? "0#{i}" : i
+  output_file = "overdue_#{num}.txt"
+  output_file_name = File.join(output_dir, output_file)
+  File.open(output_file_name, "w+") do |file|
+    file.write(output)
+  end
+  i += 1
+end
+
+puts "\n#{i} overdue letters were generated in:"
+puts output_dir
+puts "\n"
--- treasure_hunt/init.rb
+++ treasure_hunt/init.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+
 # Challenge: Treasure Hunt
 #
 # Searches the hunt_dir for files containing "treasure"
@@ -7,3 +8,37 @@
 # Outputs the file path when "treasure" is found
 # Example:
 # "Found treasure! buried_treasure/swamp/old_crate.txt"
+    # Read from list file and display numbered list
+$parent_dir = File.expand_path(File.dirname(__FILE__))
+
+def search_file(file_path)
+  unless File.exist?(file_path) 
+    puts "Failed to find #{file_path}"
+    return
+  end
+  data = File.read(file_path)
+  if data.include?("treasure")
+    new_path = file_path.gsub("#{$parent_dir}/", "")
+    puts "Found treasure! #{new_path}"
+  end
+end
+    
+def find_treasure(file_path="")
+  if File.directory?(file_path)
+    Dir.foreach(file_path).each do |entry|
+      next if entry.start_with?(".")
+      abs_path = File.join(file_path, entry)
+      if File.directory?(abs_path)
+        find_treasure(abs_path)
+      else
+        search_file(abs_path)
+      end
+    end
+  else
+    search_file(file_path)
+  end
+end
+
+search_dir = File.join($parent_dir, "buried_treasure")
+puts "Processing #{search_dir}"
+find_treasure(search_dir)
